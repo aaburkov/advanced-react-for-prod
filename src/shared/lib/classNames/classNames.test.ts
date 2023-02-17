e77530e159730e3f52971a4c316b27ab6f4fa9c1
Aleksandr Burkov <aaburkov@avito.ru>
2023-02-18 00:39:20 +0300

    init jest in project

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,19 @@
+import cn from '.';
+
+describe('classNames', () => {
+    test('success', () => {
+        expect(cn('someClass')).toBe('someClass');
+        expect(cn('someClass', undefined)).toBe('someClass');
+        expect(cn('someClass', { someClassConditionally: true })).toBe('someClass someClassConditionally');
+        expect(
+            cn('someClass', { someClassConditionally: true }, ['additional1', 'additional2']),
+        ).toBe('someClass someClassConditionally additional1 additional2');
+    });
+    test('fault', () => {
+        expect(cn('someClass') === '').toBe(false);
+        expect(cn('someClass', undefined) === 'someClass undefined').toBe(false);
+        expect(
+            cn('someClass', { someClassConditionally: true }) === 'someClass someClassConditionally true',
+        ).toBe(false);
+    });
+});
